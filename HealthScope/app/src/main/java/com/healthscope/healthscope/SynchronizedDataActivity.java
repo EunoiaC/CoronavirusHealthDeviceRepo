336f96e3e15cfea350a45f35286e70e839e41825
Aadi Yadav <68039511+Cyber-cp@users.noreply.github.com>
2020-12-15 13:53:59 -0800

    Added synchronized data

---
+++ HealthScope/app/src/main/java/com/healthscope/healthscope/SynchronizedDataActivity.java
@@ -0,0 +1,14 @@
+package com.healthscope.healthscope;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SynchronizedDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_synchronized_data);
+    }
+}
