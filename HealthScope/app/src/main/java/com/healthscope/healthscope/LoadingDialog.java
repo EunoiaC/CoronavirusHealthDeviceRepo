fc7d9a7e2b4f5ac11083e4c0915c6a7febd082f0
Aadi Yadav <68039511+Cyber-cp@users.noreply.github.com>
2020-12-15 18:14:28 -0800

    Update LoadingDialog.java

--- HealthScope/app/src/main/java/com/healthscope/healthscope/LoadingDialog.java
+++ HealthScope/app/src/main/java/com/healthscope/healthscope/LoadingDialog.java
@@ -2,7 +2,6 @@ package com.healthscope.healthscope;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.view.LayoutInflater;
 
 public class LoadingDialog {
