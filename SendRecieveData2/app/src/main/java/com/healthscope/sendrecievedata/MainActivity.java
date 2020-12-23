81c82d90d2ada78785862ce26f09d492bcdd991b
Aadi Yadav <68039511+Cyber-cp@users.noreply.github.com>
2020-12-23 11:12:50 -0800

    update

--- SendRecieveData2/app/src/main/java/com/healthscope/sendrecievedata/MainActivity.java
+++ SendRecieveData2/app/src/main/java/com/healthscope/sendrecievedata/MainActivity.java
@@ -81,14 +81,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         final Connection connection = new Connection(hc06, mUUID);
+        connection.connect();
         sendData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (hc06 != null){
-                    connection.connect();
                     if (connection.isConnected()){
                         connection.write(Integer.valueOf(input.getText().toString()));
-                        connection.closeConnection();
                     }else{
                         Toast.makeText(MainActivity.this, "Could not connect to HC-06 module", Toast.LENGTH_SHORT).show();
                     }
@@ -102,11 +101,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (hc06 != null){
-                    connection.connect();
                     if (connection.isConnected()){
                         char data = connection.read();
                         Toast.makeText(MainActivity.this, data, Toast.LENGTH_SHORT).show();
-                        connection.closeConnection();
                     }else{
                         Toast.makeText(MainActivity.this, "Could not connect to HC-06 module", Toast.LENGTH_SHORT).show();
                     }
