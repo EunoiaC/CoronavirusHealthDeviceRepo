cd36c7c4910b15e2d65bfeff48de1dbcfc26d2cc
Aadi Yadav <68039511+Cyber-cp@users.noreply.github.com>
2020-12-24 00:30:25 -0800

    added sendandrecieve using connectthread

---
+++ SendAndReceiveWithConnectThread/app/src/main/java/com/healthscope/sendandreceivewithconnectthread/BluetoothConnectionService.java
@@ -0,0 +1,223 @@
+package com.healthscope.sendandreceivewithconnectthread;
+
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.util.UUID;
+
+public class BluetoothConnectionService {
+    private static final String TAG = "BluetoothConnectionServ";
+
+    private static final String appName = "MYAPP";
+
+    private static final UUID MY_UUID_INSECURE =
+            UUID.fromString("8ce255c0-200a-11e0-ac64-0800200c9a66");
+
+    private final BluetoothAdapter mBluetoothAdapter;
+
+    private ConnectThread mConnectThread;
+    private BluetoothDevice mmDevice;
+    private UUID deviceUUID;
+    ProgressDialog mProgressDialog;
+
+    private ConnectedThread mConnectedThread;
+
+    Context mContext;
+
+    public BluetoothConnectionService(Context mContext) {
+        this.mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        this.mContext = mContext;
+        start();
+    }
+
+    private AcceptThread mInsecureAcceptThread;
+
+    private class AcceptThread extends Thread {
+        private final BluetoothServerSocket mmServerSocket;
+
+        public AcceptThread() {
+            BluetoothServerSocket tmp = null;
+
+            try {
+                tmp = mBluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord(appName, MY_UUID_INSECURE);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Toast.makeText(mContext, "Setting up server using: " + MY_UUID_INSECURE, Toast.LENGTH_SHORT).show();
+
+            mmServerSocket = tmp;
+        }
+
+        public void run() {
+            Log.d(TAG, "run: AcceptThread running");
+
+            BluetoothSocket socket = null;
+
+            try {
+                socket = mmServerSocket.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(mContext, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            if (socket == null) {
+                connected(socket, mmDevice);
+            }
+        }
+
+        public void cancel() {
+            try {
+                mmServerSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(mContext, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private class ConnectThread extends Thread {
+        private BluetoothSocket mmSocket;
+
+        public ConnectThread(BluetoothDevice device, UUID uuid) {
+            mmDevice = device;
+            deviceUUID = uuid;
+        }
+
+        public void run() {
+            BluetoothSocket tmp = null;
+
+            try {
+                tmp = mmDevice.createRfcommSocketToServiceRecord(deviceUUID);
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(mContext, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            mmSocket = tmp;
+
+            mBluetoothAdapter.cancelDiscovery();
+
+            try {
+                mmSocket.connect();
+            } catch (IOException e) {
+                Toast.makeText(mContext, e.getMessage(), Toast.LENGTH_SHORT).show();
+                try {
+                    mmSocket.close();
+                } catch (IOException ex) {
+                    Toast.makeText(mContext, ex.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            connected(mmSocket, mmDevice);
+        }
+
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Toast.makeText(mContext, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void connected(BluetoothSocket mmSocket, BluetoothDevice mmDevice) {
+        mConnectedThread = new ConnectedThread(mmSocket);
+        mConnectedThread.start();
+    }
+
+    public void write(byte[] out){
+        ConnectedThread r;
+
+        mConnectedThread.write(out);
+    }
+
+    public synchronized void start() {
+        if (mConnectThread != null) {
+            mConnectThread.cancel();
+            mConnectThread = null;
+        }
+        if (mInsecureAcceptThread == null) {
+            mInsecureAcceptThread = new AcceptThread();
+            mInsecureAcceptThread.start();
+        }
+    }
+
+    public void startClient(BluetoothDevice device, UUID uuid) {
+        mProgressDialog = ProgressDialog.show(mContext, "Connecting Bluetooth", "Please wait...", true);
+
+        mConnectThread = new ConnectThread(device, uuid);
+        mConnectThread.start();
+    }
+
+
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            mProgressDialog.dismiss();
+
+            try {
+                tmpIn = mmSocket.getInputStream();
+                tmpOut = mmSocket.getOutputStream();
+            } catch (IOException e) {
+                Toast.makeText(mContext, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];
+            int bytes;
+
+            while (true) {
+                try {
+                    bytes = mmInStream.read(buffer);
+                    String incomingMessage = new String(buffer, 0, bytes);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(mContext, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    break;
+                }
+
+            }
+        }
+
+        public void write(byte[] bytes){
+            String text = new String(bytes, Charset.defaultCharset());
+
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) {
+                Toast.makeText(mContext, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Toast.makeText(mContext, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
