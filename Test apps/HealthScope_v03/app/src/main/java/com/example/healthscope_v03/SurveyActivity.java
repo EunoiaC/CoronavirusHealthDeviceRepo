ee795b91641532f1b18daa9dea85b77176428438
Aadi Yadav <68039511+Cyber-cp@users.noreply.github.com>
2020-12-26 11:02:46 -0800

    moved ConnectedThread

---
+++ Test apps/HealthScope_v03/app/src/main/java/com/example/healthscope_v03/SurveyActivity.java
@@ -0,0 +1,14 @@
+package com.example.healthscope_v03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SurveyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey);
+    }
+}
