4d1e135929a1964bcb5953536f0eba205110b976
Aadi Yadav <68039511+Cyber-cp@users.noreply.github.com>
2020-12-28 10:30:44 -0800

    Added survey
    just copy pasted everything from the othe healthscope app

---
+++ FixSurvey/HealthScope_v03/app/src/main/java/com/example/healthscope_v03/SurveyActivity.java
@@ -0,0 +1,34 @@
+package com.example.healthscope_v03;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SurveyActivity extends AppCompatActivity {
+
+    CheckboxQuestion q1, q2, q3;
+    Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey);
+
+        q1 = findViewById(R.id.question1);
+        q2 = findViewById(R.id.question2);
+        q3 = findViewById(R.id.question3);
+        submit = findViewById(R.id.submit);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SurveyActivity.this, "Question 1 answer " + q1.getAnswer(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(SurveyActivity.this, "Question 2 answer " + q2.getAnswer(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(SurveyActivity.this, "Question 3 answer " + q3.getAnswer(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
