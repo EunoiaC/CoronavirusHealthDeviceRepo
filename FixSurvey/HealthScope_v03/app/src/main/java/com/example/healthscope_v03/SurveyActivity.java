9e2daa196ec3eaaa58a246b5977f6b43c7ceb8f3
Aadi Yadav <68039511+Cyber-cp@users.noreply.github.com>
2020-12-28 10:42:50 -0800

    Update SurveyActivity.java

--- FixSurvey/HealthScope_v03/app/src/main/java/com/example/healthscope_v03/SurveyActivity.java
+++ FixSurvey/HealthScope_v03/app/src/main/java/com/example/healthscope_v03/SurveyActivity.java
@@ -1,5 +1,6 @@
 package com.example.healthscope_v03;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,9 +26,14 @@ public class SurveyActivity extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (q1.getAnswer().equals("Not answered") || q2.getAnswer().equals("Not answered") || q3.getAnswer().equals("Not answered")){
+                    Toast.makeText(SurveyActivity.this, "Please answer all the questions.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 Toast.makeText(SurveyActivity.this, "Question 1 answer " + q1.getAnswer(), Toast.LENGTH_SHORT).show();
                 Toast.makeText(SurveyActivity.this, "Question 2 answer " + q2.getAnswer(), Toast.LENGTH_SHORT).show();
                 Toast.makeText(SurveyActivity.this, "Question 3 answer " + q3.getAnswer(), Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(SurveyActivity.this, MainActivity.class));
             }
         });
     }
