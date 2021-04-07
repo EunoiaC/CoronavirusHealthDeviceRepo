89ce8bf18090447550beb9bb5277891aa9849988
Aadi Yadav <68039511+Cyber-cp@users.noreply.github.com>
2021-04-07 13:38:16 -0700

    Added onclick for graph

--- HealthScope_v03 SurveyFixed/app/src/main/java/com/example/healthscope_v03/GraphFragment.java
+++ HealthScope_v03 SurveyFixed/app/src/main/java/com/example/healthscope_v03/GraphFragment.java
@@ -1,5 +1,6 @@
 package com.example.healthscope_v03;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,13 +9,18 @@ import androidx.fragment.app.Fragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.DataPointInterface;
 import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.OnDataPointTapListener;
+import com.jjoe64.graphview.series.Series;
 
 import java.text.Format;
 import java.text.ParseException;
@@ -39,11 +45,12 @@ public class GraphFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_graph, container, false);
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        tempDate = new Date(System.currentTimeMillis() - TimeUnit.HOURS.toMillis(72));
+        tempDate = new Date(System.currentTimeMillis() - (TimeUnit.HOURS.toMillis(71) + TimeUnit.MINUTES.toMillis(50)));
         Log.d("TAG", "startDate: " + tempDate);
 
         graphView = getView().findViewById(R.id.tempGraph);
@@ -56,16 +63,22 @@ public class GraphFragment extends Fragment {
             dates.add(tempDate);
             tempDate = new Date(tempDate.getTime() + TimeUnit.MINUTES.toMillis(10));
         }
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>(values);
         graphView.getViewport().setScrollable(true);
 //        graphView.getViewport().setMaxXAxisSize(432);
         graphView.getViewport().setMaxX(temps.length);
+        series.setOnDataPointTapListener(new OnDataPointTapListener() {
+            @Override
+            public void onTap(Series series, DataPointInterface dataPoint) {
+                Toast.makeText(getActivity(), "Clicked on: temp = " + dataPoint.getY() + " time = " + simpleDateFormat.format(dates.get((int) dataPoint.getX())), Toast.LENGTH_LONG).show();
+            }
+        });
         graphView.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
             @Override
             public String formatLabel(double value, boolean isValueX) {
                 if (isValueX) {
                     Log.d("TAG", "formatLabel: " + value);
-                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
                     Log.d("TAG", "tempDate: " + dates.get((int) value));
                     return simpleDateFormat.format(dates.get((int) value));
                 } else {
@@ -75,6 +88,9 @@ public class GraphFragment extends Fragment {
             }
         });
         graphView.getViewport().setScalable(true);
+        graphView.getViewport().setYAxisBoundsManual(true);
+        graphView.getViewport().setMaxY(40);
+        graphView.getViewport().setMinY(25);
         graphView.addSeries(series);
 
 
@@ -82,7 +98,7 @@ public class GraphFragment extends Fragment {
 
     void generateRandomTemps() {
         for (int i = 0; i < temps.length; i++) {
-            double randomNum = 4 + new Random().nextDouble() * (50 - 4);
+            double randomNum = 25 + new Random().nextDouble() * (40 - 25);
             temps[i] = (float) ((randomNum % 4) + 35);
             Log.d("TAG", "generateRandomTemps: " + temps[i] + " " + randomNum);
         }
