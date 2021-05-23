e05a67b6e8af03f47f6a594aace9ac25a1db7799
AadiYadav <68039511+Cyber-cp@users.noreply.github.com>
2021-05-23 09:42:20 -0700

    added night mode compat

--- HealthScope_v03 SurveyFixed/app/src/main/java/com/example/healthscope_v03/MainActivity.java
+++ HealthScope_v03 SurveyFixed/app/src/main/java/com/example/healthscope_v03/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myToolbar = findViewById(R.id.my_toolbar);
-        //setSupportActionBar(myToolbar);
+        setSupportActionBar(myToolbar);
         myToolbar.setTitle("HealthScope v1.0");
         myToolbar.setTitleTextColor(Color.GRAY);
 
