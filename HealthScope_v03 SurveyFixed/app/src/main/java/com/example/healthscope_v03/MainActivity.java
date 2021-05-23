5a3acc9f38998c699eb27da5c41c2f1b6ba3a4d0
blacksmith0040 <76001752+blacksmith0040@users.noreply.github.com>
2021-05-24 01:25:28 +0300

    Update MainActivity.java

--- HealthScope_v03 SurveyFixed/app/src/main/java/com/example/healthscope_v03/MainActivity.java
+++ HealthScope_v03 SurveyFixed/app/src/main/java/com/example/healthscope_v03/MainActivity.java
@@ -119,14 +119,11 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24);
         toolbar.setNavigationOnClickListener(v -> {
             if(surveyFragment.isVisible())
-                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.slide_in_right,R.anim.slide_out_left).remove(surveyFragment).commit();
+                startMainKillSurvey();
             if(graphFragment.isVisible())
-                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.slide_in_right,R.anim.slide_out_left).remove(graphFragment).commit();
+                startMainKillGraph();
             if(settingsFragment.isVisible())
-                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.slide_in_right,R.anim.slide_out_left).remove(settingsFragment).commit();
-            getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.slide_in_left,R.anim.slide_out_right).show(mainFragment).commit();
-            hideBackButton();
-            invalidateOptionsMenu();
+                startMainKillSettings();
         });
     }
 
